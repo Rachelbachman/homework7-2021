003ed12cde9ce3619f18857da79e8b2b37584d38
Rachelbachman <78323446+Rachelbachman@users.noreply.github.com>
2021-11-16 18:10:28 -0500

    Update video.js

--- js/video.js
+++ js/video.js
@@ -11,6 +11,8 @@ window.addEventListener("load", function() {
  document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
 	video.play();
+	document.querySelector('#volume').innerHTML = this.value + '%';
+	video.volume = (this.value/100)
  });
 
  document.querySelector("#pause").addEventListener("click", function() {
